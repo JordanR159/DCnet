e369f79a7aabc921a6b3b4b1e8c39634b6751127
Rajas H Karandikar <rkarandi@purdue.edu>
2017-07-07 04:52:56 -0900

    Added comments in DCnetController.py

--- DCnetController.py
+++ DCnetController.py
@@ -9,6 +9,7 @@ class   DCnetController (app_manager.RyuApp):
     def __init__ (self, *args, **kwargs):
         super(DCnetController, self).__init__(*args, **kwargs)
 
+        # All the switches in the DC and their positions
         self.switchDB = {}
         self.switchDB['128.10.135.31'] = { 'name' : 'core0', 'level' : 0, 'pod' : 0, 'column' : 0 }
         self.switchDB['128.10.135.32'] = { 'name' : 'core1', 'level' : 0, 'pod' : 0, 'column' : 1 }
@@ -20,30 +21,30 @@ class   DCnetController (app_manager.RyuApp):
         self.switchDB['128.10.135.38'] = { 'name' : 'aggr11', 'level' : 1, 'pod' : 1, 'column' : 1 }
         self.switchDB['128.10.135.39'] = { 'name' : 'edge10', 'level' : 2, 'pod' : 1, 'column' : 0 }
         self.switchDB['128.10.135.40'] = { 'name' : 'edge11', 'level' : 2, 'pod' : 1, 'column' : 1 }
+
+        # Radix of switches in the DC
         self.radix = 4
+
+        # VMs in the DC
         self.vms = [ { 'ip' : '10.0.0.1', 'mac' : 'd4:ae:52:c9:a8:34', 'edge' : 'edge00', 'rmac' : 'dc:dc:dc:00:00:00', 'port' : 1},
                      { 'ip' : '10.0.0.2', 'mac' : 'd4:ae:52:c9:ab:16', 'edge' : 'edge01', 'rmac' : 'dc:dc:dc:00:01:00', 'port' : 1},
                      { 'ip' : '10.0.0.3', 'mac' : 'd4:ae:52:c8:c3:61', 'edge' : 'edge10', 'rmac' : 'dc:dc:dc:01:00:00', 'port' : 1}]
 
+    # Handle a new switch joining
     @set_ev_cls (event.EventSwitchEnter, MAIN_DISPATCHER)
     def switch_enter_handler (self, ev):
 
         switch = ev.switch
-        """
-        for k in switch.__dict__.keys():
-            print k, switch.__dict__[k]
-        print 'Datapath fields:'
-        for k in switch.dp.__dict__.keys():
-            print k, switch.dp.__dict__[k]
-        """
         ip = switch.dp.address[0]
 
+        # Check if the switch is in our database of switches
         if ip in self.switchDB.keys():
             print 'Switch ', ip, 'connected!!'
             print 'Level: ', self.switchDB[ip]['level']
             print 'Pod: ', self.switchDB[ip]['pod']
             print 'Column: ', self.switchDB[ip]['column']
 
+            # Depending on its position, add flows in it
             if self.switchDB[ip]['level'] == 0:
                 self.add_flows_core(switch)
             elif self.switchDB[ip]['level'] == 1:
@@ -51,6 +52,7 @@ class   DCnetController (app_manager.RyuApp):
             elif self.switchDB[ip]['level'] == 2:
                 self.add_flows_edge(switch)
 
+    # Add flows in a core switch
     def add_flows_core (self, switch=None):
 
         dp = switch.dp
@@ -59,6 +61,7 @@ class   DCnetController (app_manager.RyuApp):
 
         for i in range(self.radix):
 
+            # Match the POD ID in the RMAC and forward accordingly
             match = parser.OFPMatch(eth_dst=('dc:dc:dc:%s:00:00' % (i), 'ff:ff:ff:ff:00:00'))
             action = parser.OFPActionOutput(i+1)
             instr = parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, [action])
@@ -70,6 +73,7 @@ class   DCnetController (app_manager.RyuApp):
 
             dp.send_msg(flowmod)
 
+    # Add flows in an aggregate switch
     def add_flows_aggr (self, switch=None):
 
         dp = switch.dp
@@ -79,8 +83,10 @@ class   DCnetController (app_manager.RyuApp):
         pod = self.switchDB[ip]['pod']
         column = self.switchDB[ip]['column']
 
+        # Handle flows that remain in the pod
         for i in range(self.radix/2):
 
+            # Match the pod number and the column number and forward accordingly
             dst='dc:dc:dc:{0:02x}:{1:02x}:00'.format(pod, i)
             match = parser.OFPMatch(eth_dst=(dst, 'ff:ff:ff:ff:ff:00'))
             action = parser.OFPActionOutput(i+1)
@@ -93,6 +99,8 @@ class   DCnetController (app_manager.RyuApp):
 
             dp.send_msg(flowmod)
 
+        # Handle flows that are destined to other pods
+        # ECMP the flow towards the core switches
         match = parser.OFPMatch(eth_dst=('dc:dc:dc:00:00:00', 'ff:ff:ff:00:00:00'))
         action = parser.NXActionBundle(algorithm=nicira_ext.NX_BD_ALG_HRW,
                                        fields=nicira_ext.NX_HASH_FIELDS_SYMMETRIC_L4,
@@ -111,6 +119,7 @@ class   DCnetController (app_manager.RyuApp):
 
         dp.send_msg(flowmod)
 
+    # Add flows in an edge switch
     def add_flows_edge (self, switch=None):
 
         dp = switch.dp
@@ -122,7 +131,10 @@ class   DCnetController (app_manager.RyuApp):
 
         for v in self.vms:
 
+            # Flows destined to a VM under this switch
             if v['edge'] == self.switchDB[ip]['name']:
+
+                # If the Ethernet destination is the MAC of the VM just forward it
                 match = parser.OFPMatch(eth_dst=v['mac'])
                 action = parser.OFPActionOutput(v['port'])
                 instr = parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, [action])
@@ -134,6 +146,7 @@ class   DCnetController (app_manager.RyuApp):
 
                 dp.send_msg(flowmod)
 
+                # If the Ethernet destination is the RMAC, restore the MAC and forward
                 match = parser.OFPMatch(eth_dst=v['rmac'])
                 action1 = parser.OFPActionSetField(eth_dst=v['mac'])
                 action2 = parser.OFPActionOutput(v['port'])
@@ -145,7 +158,9 @@ class   DCnetController (app_manager.RyuApp):
                                             instructions=[instr])
 
                 dp.send_msg(flowmod)
-            else:
+            else: # For VMs not under this switch
+
+                # Rewrite the Ethernet destination with the RMAC and ECMP towards the aggregates
                 match = parser.OFPMatch(eth_dst=v['mac'])
                 action1 = parser.OFPActionSetField(eth_dst=v['rmac'])
                 action2 = parser.NXActionBundle(algorithm=nicira_ext.NX_BD_ALG_HRW,
