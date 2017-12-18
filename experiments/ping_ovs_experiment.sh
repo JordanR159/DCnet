ad9e2c09c3892f4be02b89061de2b40134b0cc01
Rajas H Karandikar <rkarandi@purdue.edu>
2017-12-17 19:41:59 -1000

    Added ping_ovs results and helper scripts

--- experiments/ping_ovs_experiment.sh
+++ experiments/ping_ovs_experiment.sh
@@ -16,20 +16,24 @@ echo -e "#ping_size\tno-rewriting\trewriting" > ${output_dir}/plotdata
 for s in ${sizes[*]}
 do
 	# Perform single ping to load OVS caches
-	ping6 n111_1 -c 1
+	ping6 n111_1 -c 1 > /dev/null
 
 	# Perform experiment without rewriting
 	output_file=${output_dir}/ping_s${s}_output
 	sudo ping6 n111_1 -i 0.01 -c 100 -s $s > $output_file
 	avg1=`tail -1 $output_file | gawk '{print $4}' | gawk 'BEGIN {FS = "/"}; {print $2}'`
+	min1=`tail -1 $output_file | gawk '{print $4}' | gawk 'BEGIN {FS = "/"}; {print $1}'`
+	max1=`tail -1 $output_file | gawk '{print $4}' | gawk 'BEGIN {FS = "/"}; {print $3}'`
 
 	# Perform single ping to load OVS caches
-	ping6 n111_r_1 -c 1
+	ping6 n111_r_1 -c 1 > /dev/null
 
 	# Perform experiment with rewriting
 	output_file=${output_dir}/ping_s${s}_r_output
 	sudo ping6 n111_r_1 -i 0.01 -c 100 -s $s > $output_file
 	avg2=`tail -1 $output_file | gawk '{print $4}' | gawk 'BEGIN {FS = "/"}; {print $2}'`
+	min2=`tail -1 $output_file | gawk '{print $4}' | gawk 'BEGIN {FS = "/"}; {print $1}'`
+	max2=`tail -1 $output_file | gawk '{print $4}' | gawk 'BEGIN {FS = "/"}; {print $3}'`
 
 	echo -e "${s}\t${avg1}\t${avg2}" >> $output_dir/plotdata
 done
