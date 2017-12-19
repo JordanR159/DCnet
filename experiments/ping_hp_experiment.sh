85b71412e06c7413b6653aa3203e92dc3349e7f6
Rajas H Karandikar <rkarandi@purdue.edu>
2017-12-18 20:18:59 -1000

    Added ping_hp results

--- experiments/ping_hp_experiment.sh
+++ experiments/ping_hp_experiment.sh
@@ -9,6 +9,9 @@ step=200
 sizes=(`seq $first $step $last`)
 sizes+=(8952)
 
+n102_1="dc98::9898:9800:d2"
+n102_r_1="dc99::9999:9900:d2"
+
 mkdir -p $output_dir
 rm -rf ${output_dir}/*
 echo -e "#ping_size\tno-rewriting\trewriting" > ${output_dir}/plotdata
@@ -16,19 +19,19 @@ echo -e "#ping_size\tno-rewriting\trewriting" > ${output_dir}/plotdata
 for s in ${sizes[*]}
 do
 	# Perform single ping to load caches
-	ping6 n107_1 -c 1
+	ssh nebula101 "ping6 -c 1 $n102_1" > /dev/null
 
 	# Perform experiment without rewriting
 	output_file=${output_dir}/ping_s${s}_output
-	sudo ping6 n107_1 -i 0.01 -c 100 -s $s > $output_file
+	ssh nebula101 "sudo ping6 $n102_1 -i 0.01 -c 100 -s $s" > $output_file
 	avg1=`tail -1 $output_file | gawk '{print $4}' | gawk 'BEGIN {FS = "/"}; {print $2}'`
 
 	# Perform single ping to load caches
-	ping6 n107_r_1 -c 1
+	ssh nebula101 "ping6 -c1 $n102_r_1" > /dev/null
 
 	# Perform experiment with rewriting
 	output_file=${output_dir}/ping_s${s}_r_output
-	sudo ping6 n107_r_1 -i 0.01 -c 100 -s $s > $output_file
+	ssh nebula101 "sudo ping6 $n102_r_1 -i 0.01 -c 100 -s $s" > $output_file
 	avg2=`tail -1 $output_file | gawk '{print $4}' | gawk 'BEGIN {FS = "/"}; {print $2}'`
 
 	echo -e "${s}\t${avg1}\t${avg2}" >> $output_dir/plotdata
