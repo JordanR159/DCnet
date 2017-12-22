536cc00ecd7ebef7041c7ef1f6b8e6ac5032df6e
Rajas H Karandikar <rkarandi@purdue.edu>
2017-12-22 11:43:19 -1000

    Added tcp control1 experiment results and related scripts. Added set_mtu_all.sh to set MTU for all DCnet switches and servers

--- experiments/tcp_control1_experiment.sh
+++ experiments/tcp_control1_experiment.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Remove the previous output and create a new directory
 rm -rf tcp_control1_output/
 mkdir tcp_control1_output
+
+# Kill any previously running iperf3 servers
+for p in $(ps -aef | grep rajas | grep "iperf3 -s" | grep -v grep | gawk '{print $2}')
+do
+	kill -9 $p
+done
 
 # Start the iperf3 servers at two different ports
 iperf3 -s -B dc98::9898:9800:4 -p 10001 --logfile tcp_control1_output/srv_rawop1 -D
@@ -10,19 +17,27 @@ iperf3 -s -B dc98::9898:9800:4 -p 10002 --logfile tcp_control1_output/srv_rawop2
 plotdata_file="tcp_control1_output/plotdata"
 echo -e "#Delay(us)\tno-delay\tdelay" > $plotdata_file
 
+# Delay values to test
 delays=(0 200 400 600 800 1000 1200)
 
+# Lines to skip in the raw output to get to the first run output
 skip=19
+
 for d in ${delays[*]}
 do
 	echo "delay is $d us"
+
+	# Set the delay in nebula106
 	ssh nebula106 "sudo tc qdisc change dev enp1s0f2 root handle 0: netem delay ${d}us"
 
-	ssh -f nebula111 "iperf3 -c dc98::9898:9800:4 -p 10001 -O 10 -t 15"
-	ssh -f nebula112 "iperf3 -c dc98::9898:9800:4 -p 10002 -O 10 -t 15"
+	# Start the iperf3 clients
+	ssh -f nebula111 "iperf3 -c dc98::9898:9800:4 -p 10001 -O 10 -t 15 > /dev/null"
+	ssh -f nebula112 "iperf3 -c dc98::9898:9800:4 -p 10002 -O 10 -t 15 > /dev/null"
 
+	# Wait until the test finishes
 	sleep 30
 
+	# Compute the average for client1
 	total=0
 	avg1=0
 	for val in $(cat tcp_control1_output/srv_rawop1 | tail -n +$skip | head -n 10 | gawk '{print $7}')
@@ -32,6 +47,7 @@ do
 	done
 	avg1=$(echo $total/10 | bc -l)
 
+	# Compute the average for client 2
 	total=0
 	for val in $(cat tcp_control1_output/srv_rawop2 | tail -n +$skip | head -n 10 | gawk '{print $7}')
 	do
@@ -40,12 +56,15 @@ do
 	done
 	avg2=$(echo $total/10 | bc -l)
 
+	# Write the values in the plotdata file
 	echo -e "${d}\t${avg1}\t${avg2}" >> $plotdata_file
 
+	# Lines to skip in the raw output to get to the next run
 	skip=`expr $skip + 36`
 done
 
-for p in $(ps -aef | grep "iperf3 -s" | grep -v grep | gawk '{print $2}')
+# Kill the server
+for p in $(ps -aef | grep rajas | grep "iperf3 -s" | grep -v grep | gawk '{print $2}')
 do
 	kill -9 $p
 done
