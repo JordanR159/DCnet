5296362b0c853348e7e1219fc4ebac33e8e90090
Rajas H Karandikar <rkarandi@purdue.edu>
2018-06-23 00:21:53 -0400

    Removed runs 3,4 for experiment vmmig_ping

--- experiments/results/vmmig_ping/run4/extract_plotdata2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat resptimes_opt_t6000_3 | head -n 10000 | tail -n 5000 | gawk '{print NR " " $4}' > plotdata2_opt
-sed -i 's/.*-1//' plotdata2_opt
-cat resptimes_noopt_t6000_3 | head -n 10000 | tail -n 5000 | gawk '{print NR " " $4}' > plotdata2_noopt
-sed -i 's/.*-1//' plotdata2_noopt
