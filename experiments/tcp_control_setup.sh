769d4ca8760a809c7968136a647d94a9db9003e3
Rajas H Karandikar <rkarandi@purdue.edu>
2018-08-16 06:04:40 -0900

    Modified ping_hp_plotscript, etc

--- experiments/tcp_control_setup.sh
+++ experiments/tcp_control_setup.sh
@@ -20,7 +20,7 @@
 # and n106 enp1s0f2, for which it is netem and variable delay
 
 # First run rewriting_ovs_setup
-./rewriting_ovs_setup.sh
+#./rewriting_ovs_setup.sh
 
 # Now set qdiscs for all switches
 
@@ -38,8 +38,8 @@ ssh nebula102 "sudo tc qdisc del dev eth8 root handle 0:; sudo tc qdisc add dev 
 echo n103
 ssh nebula103 "sudo tc qdisc del dev eth6 root handle 0:; sudo tc qdisc add dev eth6 root handle 0: pfifo limit 1000"
 ssh nebula103 "sudo tc qdisc del dev eth7 root handle 0:; sudo tc qdisc add dev eth7 root handle 0: pfifo limit 1000"
-ssh nebula103 "sudo tc qdisc del dev rename71 root handle 0:; sudo tc qdisc add dev rename71 root handle 0: pfifo limit 500"
-ssh nebula103 "sudo tc qdisc del dev rename72 root handle 0:; sudo tc qdisc add dev rename72 root handle 0: pfifo limit 1000"
+ssh nebula103 "sudo tc qdisc del dev rename20 root handle 0:; sudo tc qdisc add dev rename20 root handle 0: pfifo limit 1000"
+ssh nebula103 "sudo tc qdisc del dev rename21 root handle 0:; sudo tc qdisc add dev rename21 root handle 0: pfifo limit 1000"
 
 # nebula104
 echo n104
@@ -53,7 +53,8 @@ for i in `seq 1 4`; do ssh nebula105 "sudo tc qdisc del dev p1p$i root handle 0:
 echo n106
 ssh nebula106 "sudo tc qdisc del dev enp1s0f0 root handle 0:; sudo tc qdisc add dev enp1s0f0 root handle 0: pfifo limit 1000"
 ssh nebula106 "sudo tc qdisc del dev enp1s0f1 root handle 0:; sudo tc qdisc add dev enp1s0f1 root handle 0: pfifo limit 1000"
-ssh nebula106 "sudo tc qdisc del dev enp1s0f2 root handle 0:; sudo tc qdisc add dev enp1s0f2 root handle 0: netem delay 0ms"
+#ssh nebula106 "sudo tc qdisc del dev enp1s0f2 root handle 0:; sudo tc qdisc add dev enp1s0f2 root handle 0: netem delay 0ms"
+ssh nebula106 "sudo tc qdisc del dev enp1s0f2 root handle 0:; sudo tc qdisc add dev enp1s0f2 root handle 0: pfifo limit 1000"
 ssh nebula106 "sudo tc qdisc del dev enp1s0f3 root handle 0:; sudo tc qdisc add dev enp1s0f3 root handle 0: pfifo limit 1000"
 
 # nebula107
@@ -70,18 +71,21 @@ for i in `seq 1 4`; do ssh nebula110 "sudo tc qdisc del dev p1p$i root handle 0:
 
 # nebula111
 echo n111
-ssh nebula111 "sudo tc qdisc del dev hyp root handle 0:; sudo tc qdisc add dev hyp root handle 0: pfifo limit 1000"
-ssh nebula111 "sudo tc qdisc del dev hyp-conn root handle 0:; sudo tc qdisc add dev hyp-conn root handle 0: pfifo limit 1000"
+#ssh nebula111 "sudo tc qdisc del dev hyp root handle 0:; sudo tc qdisc add dev hyp root handle 0: pfifo limit 1000"
+#ssh nebula111 "sudo tc qdisc del dev hyp-conn root handle 0:; sudo tc qdisc add dev hyp-conn root handle 0: pfifo limit 1000"
 ssh nebula111 "sudo tc qdisc del dev eth1 root handle 0:; sudo tc qdisc add dev eth1 root handle 0: pfifo limit 1000"
+ssh nebula111 "sudo tc qdisc del dev dcnet-srv000 root handle 0:; sudo tc qdisc add dev dcnet-srv000 root handle 0: pfifo limit 1000"
 
 # nebula112
 echo n112
-ssh nebula112 "sudo tc qdisc del dev hyp root handle 0:; sudo tc qdisc add dev hyp root handle 0: pfifo limit 1000"
-ssh nebula112 "sudo tc qdisc del dev hyp-conn root handle 0:; sudo tc qdisc add dev hyp-conn root handle 0: pfifo limit 1000"
+#ssh nebula112 "sudo tc qdisc del dev hyp root handle 0:; sudo tc qdisc add dev hyp root handle 0: pfifo limit 1000"
+#ssh nebula112 "sudo tc qdisc del dev hyp-conn root handle 0:; sudo tc qdisc add dev hyp-conn root handle 0: pfifo limit 1000"
 ssh nebula112 "sudo tc qdisc del dev eno2 root handle 0:; sudo tc qdisc add dev eno2 root handle 0: pfifo limit 1000"
+ssh nebula112 "sudo tc qdisc del dev dcnet-srv010 root handle 0:; sudo tc qdisc add dev dcnet-srv010 root handle 0: pfifo limit 1000"
 
 # nebula113
 echo n113
-sudo tc qdisc del dev hyp root handle 0:; sudo tc qdisc add dev hyp root handle 0: pfifo limit 1000
-sudo tc qdisc del dev hyp-conn root handle 0:; sudo tc qdisc add dev hyp-conn root handle 0: pfifo limit 1000
+#sudo tc qdisc del dev hyp root handle 0:; sudo tc qdisc add dev hyp root handle 0: pfifo limit 1000
+#sudo tc qdisc del dev hyp-conn root handle 0:; sudo tc qdisc add dev hyp-conn root handle 0: pfifo limit 1000
 sudo tc qdisc del dev em2 root handle 0:; sudo tc qdisc add dev em2 root handle 0: pfifo limit 1000
+sudo tc qdisc del dev dcnet-srv100 root handle 0:; sudo tc qdisc add dev dcnet-srv100 root handle 0: pfifo limit 1000
