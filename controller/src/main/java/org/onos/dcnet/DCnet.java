e1c55256a74ce1394b909953eced08172eb41fb1
William Reed <reed226@purdue.edu>
2019-07-21 21:08:31 -0400

    Add basic ONOS app to serve as skeleton

---
+++ controller/src/main/java/org/onos/dcnet/DCnet.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2015 Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onos.dcnet;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.ReferenceCardinality;
+import org.onlab.packet.Ethernet;
+import org.onlab.packet.IPv4;
+import org.onlab.packet.MacAddress;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.DefaultTrafficTreatment;
+import org.onosproject.net.flow.FlowRule;
+import org.onosproject.net.flow.FlowRuleEvent;
+import org.onosproject.net.flow.FlowRuleListener;
+import org.onosproject.net.flow.FlowRuleService;
+import org.onosproject.net.flow.TrafficSelector;
+import org.onosproject.net.flow.TrafficTreatment;
+import org.onosproject.net.flow.criteria.Criterion;
+import org.onosproject.net.flow.criteria.EthCriterion;
+import org.onosproject.net.flowobjective.DefaultForwardingObjective;
+import org.onosproject.net.flowobjective.FlowObjectiveService;
+import org.onosproject.net.flowobjective.ForwardingObjective;
+import org.onosproject.net.packet.PacketContext;
+import org.onosproject.net.packet.PacketPriority;
+import org.onosproject.net.packet.PacketProcessor;
+import org.onosproject.net.packet.PacketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static org.onosproject.net.flow.FlowRuleEvent.Type.RULE_REMOVED;
+import static org.onosproject.net.flow.criteria.Criterion.Type.ETH_SRC;
+
+/**
+ * Sample application that permits only one ICMP ping per minute for a unique
+ * src/dst MAC pair per switch.
+ */
+@Component(immediate = true)
+public class DCnet {
+
+	private static Logger log = LoggerFactory.getLogger(DCnet.class);
+
+	private static final int PRIORITY = 50000;
+	private static final int DROP_PRIORITY = 50000;
+
+	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+	protected CoreService coreService;
+
+	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+	protected FlowObjectiveService flowObjectiveService;
+
+	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+	protected FlowRuleService flowRuleService;
+
+	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+	protected PacketService packetService;
+
+	private ApplicationId appId;
+	private final PacketProcessor packetProcessor = new PingPacketProcessor();
+
+	// Selector for ICMP traffic that is to be intercepted
+	private final TrafficSelector intercept = DefaultTrafficSelector.builder()
+			.matchEthType(Ethernet.TYPE_IPV4).matchIPProtocol(IPv4.PROTOCOL_ICMP)
+			.build();
+
+	@Activate
+	public void activate() {
+		appId = coreService.registerApplication("org.onosproject.dcnet",
+										() -> log.info("DCnet down"));
+		packetService.addProcessor(packetProcessor, PRIORITY);
+		packetService.requestPackets(intercept, PacketPriority.CONTROL, appId,
+									 Optional.empty());
+		log.info("Started");
+	}
+
+	@Deactivate
+	public void deactivate() {
+		packetService.removeProcessor(packetProcessor);
+		flowRuleService.removeFlowRulesById(appId);
+		log.info("Stopped");
+	}
+
+	// Processes the specified ICMP ping packet.
+	private void processPacket(PacketContext context, Ethernet eth) {
+		DeviceId deviceId = context.inPacket().receivedFrom().deviceId();
+		MacAddress src = eth.getSourceMAC();
+		MacAddress dst = eth.getDestinationMAC();
+		log.info(dst.toString());
+		if (dst.toString().equals("00:0E:C6:D7:38:63")) {
+			log.info("Dropping Packet");
+			TrafficSelector selector = DefaultTrafficSelector.builder()
+					.matchEthSrc(src).matchEthDst(dst).build();
+
+			TrafficTreatment drop = DefaultTrafficTreatment.builder()
+					.drop().build();
+
+			flowObjectiveService.forward(deviceId, DefaultForwardingObjective.builder()
+					.fromApp(appId)
+					.withSelector(selector)
+					.withTreatment(drop)
+					.withFlag(ForwardingObjective.Flag.VERSATILE)
+					.withPriority(DROP_PRIORITY)
+					.add());
+		}
+	}
+
+
+	// Indicates whether the specified packet corresponds to ICMP ping.
+	private boolean isIcmpPing(Ethernet eth) {
+		return eth.getEtherType() == Ethernet.TYPE_IPV4 &&
+				((IPv4) eth.getPayload()).getProtocol() == IPv4.PROTOCOL_ICMP;
+	}
+
+
+	// Intercepts packets
+	private class PingPacketProcessor implements PacketProcessor {
+		@Override
+		public void process(PacketContext context) {
+			Ethernet eth = context.inPacket().parsed();
+			processPacket(context, eth);
+		}
+	}
+}
